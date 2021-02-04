3605f4cd870f68f8515b0ea739c67328faee12c2
Orignal42 <rbarriquand@gmail.com>
2021-02-04 15:23:49 +0100

    fonction fizzbuzz

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -8,7 +8,11 @@
 
 var fizzbuzz = function (x) {
 //
-// YOUR CODE GOES HERE
+// for(i=1; i<=100;i++){
+  let out='';
+  if(i%3===0) out+='fizz';
+  if(i%5===0) out+='buzz';
+  console.log (out||i);
 //
 };
 
